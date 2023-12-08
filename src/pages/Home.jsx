6e7023914d45eb2f4fe101ba5e0badd790eb7f94
petermirithu <peter_mirithu@yahoo.com>
2023-12-08 19:05:33 +0300

    added an about us section with an image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ import {
     Container,
     Stack,
     Button,
+    Grid,
+    Paper,
 } from '@mui/material';
 import CategoryIcon from '@mui/icons-material/Category';
 
@@ -56,6 +58,61 @@ export default function Home() {
                         <Button variant="outlined">Our Services</Button>
                         <Button variant="outlined">Contacts</Button>
                     </Stack>
+                </Container>
+
+                <Container style={{ marginTop: 50 }}>
+                    <Grid container component="main" sx={{ height: '50vh' }}>
+                        <Grid
+                            item
+                            xs={false}
+                            sm={6}
+                            md={6}
+                            sx={{
+                                backgroundImage: 'url(tree_and_sun.jpeg)',
+                                backgroundRepeat: 'no-repeat',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                            }}
+                        />
+                        <Grid item xs={12} sm={6} md={6} component={Paper} elevation={6} square>
+                            <Box
+                                sx={{
+                                    my: 8,
+                                    mx: 4,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                }}
+                            >
+                                <Typography
+                                    component="h4"
+                                    variant="h5"
+                                    color="text.primary"
+                                    gutterBottom
+                                >
+                                    About Green Horizon Initiative
+                                </Typography>
+                                <Typography variant="body1">
+                                    Welcome to Green Horizon Initiative, where passion for the planet meets the power of community. At Green Horizon, we believe in the transformative potential of trees and the profound impact they can have on our environment and collective well-being.
+                                </Typography>
+
+                                <Typography component="h5" fontWeight={"bold"} style={{ marginTop: 10 }}>🌍 Our Vision</Typography>
+                                <Typography variant="body1">
+                                    We envision a world where urban landscapes flourish with lush greenery, where communities actively engage in tree-centric initiatives, and where sustainable practices become a way of life. Through our projects, we aim to create lasting impressions on the Earth and in the hearts of those who join us.
+                                </Typography>
+
+                                <Typography component="h5" fontWeight={"bold"} style={{ marginTop: 10 }}>🌱 Our Mission</Typography>
+                                <Typography variant="body1">
+                                    Our mission is to foster a deeper connection between people and nature by spearheading innovative planting projects. From pioneering Urban Tree Mapping endeavors to leveraging cutting-edge technology for Tree Identification Apps, we are committed to empowering individuals to be stewards of the environment.
+                                </Typography>
+
+                                <Typography component="h5" fontWeight={"bold"} style={{ marginTop: 10 }}>🤝 Community-Centric Approach</Typography>
+                                <Typography variant="body1">
+                                    Green Horizon Initiative is more than just a collection of projects; it's a vibrant community of individuals passionate about making a positive impact. We believe that true change happens when communities come together with a shared purpose. Join our growing family of green warriors and be part of a movement that transcends borders.
+                                </Typography>
+
+                            </Box>
+                        </Grid>
+                    </Grid>
                 </Container>
             </Box>
 
